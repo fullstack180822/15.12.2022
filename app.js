df10330b170e2e5a7c957d5d1a025660ee1916b1
micserv010821 <dotnet290320@gmail.com>
2022-12-15 19:57:31 +0200

    upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const calc = require('./calc')
+const { add } = require('./calc')
+const { pi } = require('./calc')
+
+calc.add(2, 5);
+add(3, 4);
+console.log(pi);
---
+++ calc.js
@@ -0,0 +1,8 @@
+
+function plus(x,y) { 
+    console.log(`${x} + ${y} = ${x+y}`);
+}
+
+
+module.exports.add = plus
+module.exports.pi = 3.14
